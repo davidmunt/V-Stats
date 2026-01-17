3d053f6de7574dcdf1b1ca993e1dc3cbac30466b
David <davidmunteanonti@gmail.com>
2026-01-18 00:14:38 +0100

    Cambio version react y soluciono error guard

--- frontend/src/contexts/auth/AuthProvider.tsx
+++ frontend/src/contexts/auth/AuthProvider.tsx
@@ -1,6 +1,8 @@
 import type { ReactNode } from "react";
 import { useCurrentUserQuery } from "@/queries/auth/useCurrentUser";
 import { AuthContext } from "./AuthContext";
+import tokenService from "@/lib/token";
+import { ACCESS_TOKEN_KEY } from "@/constants/token.constant";
 
 type Props = {
   children: ReactNode;
@@ -8,12 +10,13 @@ type Props = {
 
 const AuthProvider = ({ children }: Props) => {
   const { data: user, isLoading } = useCurrentUserQuery();
+  const token = tokenService.getToken(ACCESS_TOKEN_KEY);
 
   return (
     <AuthContext.Provider
       value={{
         user: user ?? null,
-        isAuthenticated: !!user,
+        isAuthenticated: !!token,
         isLoading,
       }}
     >
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path"; // 1. Importamos path
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      // 2. Definimos que @ apunte a la carpeta src
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "src"),
     },
+    dedupe: ["react", "react-dom"],
   },
 });
