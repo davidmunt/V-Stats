cdc6a341200280c4cc5cd95817975c0d1e22a577
David <davidmunteanonti@gmail.com>
2026-02-05 01:56:47 +0100

    Backend Spring: Añado CORS al backend de Spring Boot

--- backend-springboot/src/main/java/com/vstats/vstats/infrastructure/configuration/OpenApiConfig.java
+++ backend-springboot/src/main/java/com/vstats/vstats/infrastructure/configuration/OpenApiConfig.java
@@ -2,7 +2,6 @@ package com.vstats.vstats.infrastructure.configuration;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.Contact;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
